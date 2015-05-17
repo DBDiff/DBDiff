566dc13411d7b34e998e1e72d5d9705c98daf014
Zoli Issam <jawbfl@gmail.com>
2015-05-17 21:54:58 +0100

    fix queries for new column

--- src/DB/Data/LocalTableData.php
+++ src/DB/Data/LocalTableData.php
@@ -30,16 +30,17 @@ class LocalTableData {
         $db1 = $this->source->getDatabaseName();
         $db2 = $this->target->getDatabaseName();
 
-        $columns = $this->manager->getColumns('source', $table);
-        
+        $columns1 = $this->manager->getColumns('source', $table);
+        $columns2 = $this->manager->getColumns('target', $table);
+
         $wrapConvert = function($arr, $p) {
             return array_map(function($el) use ($p) {
-                return "CONVERT(`{$p}`.`{$el}` USING utf8)";
+                return "CONVERT(`{$p}`.`{$el}` USING utf8) as `{$el}`";
             }, $arr);
         };
 
-        $columnsAUtf = implode(',', $wrapConvert($columns, 'a'));
-        $columnsBUtf = implode(',', $wrapConvert($columns, 'b'));
+        $columnsAUtf = implode(',', $wrapConvert($columns1, 'a'));
+        $columnsBUtf = implode(',', $wrapConvert($columns2, 'b'));
 
         $keyCols = implode(' AND ', array_map(function($el) {
             return "`a`.`{$el}` = `b`.`{$el}`";
@@ -54,28 +55,29 @@ class LocalTableData {
         $keyNulls2 = implode(' AND ', $keyNull($key, 'b'));
 
         $this->source->setFetchMode(\PDO::FETCH_NAMED);
-        $result = $this->source->select(
-           "SELECT $columnsAUtf,'source' AS _connection FROM {$db1}.{$table} as a
+        $result1 = $this->source->select(
+           "SELECT $columnsAUtf FROM {$db1}.{$table} as a
             LEFT JOIN {$db2}.{$table} as b ON $keyCols WHERE $keyNulls2
-            UNION ALL
-            SELECT $columnsBUtf,'target' AS _connection FROM {$db2}.{$table} as b
+        ");
+        $result2 = $this->source->select(
+           "SELECT $columnsBUtf FROM {$db2}.{$table} as b
             LEFT JOIN {$db1}.{$table} as a ON $keyCols WHERE $keyNulls1
         ");
         $this->source->setFetchMode(\PDO::FETCH_ASSOC);
 
-        foreach ($result as $row) {
-            if ($row['_connection'] == 'source') {
-                $diffSequence[] = new InsertData($table, [
-                    'keys' => array_only($row, $key),
-                    'diff' => new \Diff\DiffOp\DiffOpAdd(array_except($row, '_connection'))
-                ]);
-            } else if ($row['_connection'] == 'target') {
-                $diffSequence[] = new DeleteData($table, [
-                    'keys' => array_only($row, $key),
-                    'diff' => new \Diff\DiffOp\DiffOpRemove(array_except($row, '_connection'))
-                ]);
-            }
+        foreach ($result1 as $row) {
+            $diffSequence[] = new InsertData($table, [
+                'keys' => array_only($row, $key),
+                'diff' => new \Diff\DiffOp\DiffOpAdd(array_except($row, '_connection'))
+            ]);
         }
+        foreach ($result2 as $row) {
+            $diffSequence[] = new DeleteData($table, [
+                'keys' => array_only($row, $key),
+                'diff' => new \Diff\DiffOp\DiffOpRemove(array_except($row, '_connection'))
+            ]);
+        }
+
         return $diffSequence;
     }
 
@@ -85,7 +87,8 @@ class LocalTableData {
         $db1 = $this->source->getDatabaseName();
         $db2 = $this->target->getDatabaseName();
 
-        $columns = $this->manager->getColumns('source', $table);
+        $columns1 = $this->manager->getColumns('source', $table);
+        $columns2 = $this->manager->getColumns('target', $table);
         
         $wrapAs = function($arr, $p1, $p2) {
             return array_map(function($el) use ($p1, $p2) {
@@ -99,10 +102,10 @@ class LocalTableData {
             }, $arr);
         };
 
-        $columnsAas = implode(',', $wrapAs($columns, 'a', 's_'));
-        $columnsA   = implode(',', $wrapCast($columns, 'a'));
-        $columnsBas = implode(',', $wrapAs($columns, 'b', 't_'));
-        $columnsB   = implode(',', $wrapCast($columns, 'b'));
+        $columnsAas = implode(',', $wrapAs($columns1, 'a', 's_'));
+        $columnsA   = implode(',', $wrapCast($columns1, 'a'));
+        $columnsBas = implode(',', $wrapAs($columns2, 'b', 't_'));
+        $columnsB   = implode(',', $wrapCast($columns2, 'b'));
         
         $keyCols = implode(' AND ', array_map(function($el) {
             return "a.{$el} = b.{$el}";
@@ -125,10 +128,16 @@ class LocalTableData {
                     $theKey = substr($k, 2);
                     $targetKey = 't_'.$theKey;
                     $sourceValue = $value;
-                    $targetValue = $row[$targetKey];
+                    
                     if (in_array($theKey, $key)) $keys[$theKey] = $value;
-                    if ($sourceValue != $targetValue) {
-                        $diff[$theKey] = new \Diff\DiffOp\DiffOpChange($targetValue, $sourceValue);
+                    
+                    if (isset($row[$targetKey])) {
+                        $targetValue = $row[$targetKey];
+                        if ($sourceValue != $targetValue) {
+                            $diff[$theKey] = new \Diff\DiffOp\DiffOpChange($targetValue, $sourceValue);
+                        }
+                    } else {
+                        $diff[$theKey] = new \Diff\DiffOp\DiffOpChange(NULL, $sourceValue);
                     }
                 }
             }
