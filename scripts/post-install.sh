e847e92104a9c464226e44d24eaa0bca1f6dca94
Jasdeep <jhbsk@users.noreply.github.com>
2019-04-09 13:15:51 +0100

    type(chore): add ways to support dbdiff & status badges

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 text="
-Thank you for installing \e[1mDBDiff\e[0m!
+Thank you for installing \x1B[1mDBDiff\x1B[0m!
 
-DBDiff is MIT-licensed, with development \e[1mfunded entirely by the community\e[0m.
+DBDiff is MIT-licensed, with development \x1B[1mfunded entirely by the community\x1B[0m.
 
 So if you've found DBDiff useful, please consider:
 
-* \e[1mBecoming a backer or sponsor on Patreon\e[0m: \e[1;4mhttps://www.patreon.com/dbdiff\e[0m
+* \x1B[1mBecoming a backer or sponsor on Patreon\x1B[0m: \x1B[1;4mhttps://www.patreon.com/dbdiff\x1B[0m
 
-* Giving a \e[1mone-time donation via PayPal\e[0m: \e[1;4mhttps://www.paypal.me/dbdiff\e[0m
- 
-* If you're interested in \e[1mcommercial support\e[0m packages, please get in touch: \e[1;4mhttps://akalsoftware.com/contact-us/\e[0m
- 
-Kindly spare 2 minutes to give us your feedback on DBDiff: \e[1;4mhttps://forms.gle/gjdJxZxdVsz7BRxg7\e[0m
- 
-Made with \e[1m\e[91mlove\e[0m by \e[1mAkalSoftware.com\e[0m
+* Giving a \x1B[1mone-time donation via PayPal\x1B[0m: \x1B[1;4mhttps://www.paypal.me/dbdiff\x1B[0m
+
+* If you're interested in \x1B[1mcommercial support\x1B[0m packages, please get in touch: \x1B[1;4mhttps://akalsoftware.com/contact-us/\x1B[0m
+
+Kindly spare 2 minutes to give us your feedback on DBDiff: \x1B[1;4mhttps://forms.gle/gjdJxZxdVsz7BRxg7\x1B[0m
+
+Made with \x1B[1m\x1B[91mlove\x1B[0m by \x1B[1mAkalSoftware.com\x1B[0m
 
 "
 
